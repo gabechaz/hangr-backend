72c3d05e3097013b8736040c5f9e49697e5036d4
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-02-27 13:37:06 -0500

    seeds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+    before_action :authenticate, only: [:me]
 
     def show
         user = User.find(params[:id])
@@ -10,12 +10,23 @@ class UsersController < ApplicationController
         user = User.find_by(username: params[:username])
     
         if user && user.authenticate(params[:password])
+        token = JWT.encode({user_id: user.id}, 'codename', 'HS256')
           @current_user = user
-          render json: user
+          render json: {user: user, token: token}
         else
-          render json: {errors: ['Try again loser!']}
+          render json: {errors: ['Invalid username or password']}
         end
     
+      end
+
+      def signup
+        new_user = User.create(user_params)
+        if new_user.id? 
+        token = JWT.encode({user_id: user.id}, 'codename', 'HS256')
+        render json: {user: new_user, token: token}, status: :created
+        else
+            render json: new_user.errors.full_messages
+        end
       end
 
 
@@ -26,5 +37,10 @@ class UsersController < ApplicationController
       def me
         user = @current_user
         render json: user
+      end
+      
+      private
+      def user_params
+        params.permit(:name, :username, :password)
       end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
 get '/users/:id', to: 'users#show'
-
 post "/login", to: "users#login"
-
+post '/signup', to: 'users#signup'
 post "/logout", to: "users#logout"
-
+get '/me', to: 'users#me'
 end
